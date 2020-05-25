0638f116bd496c84825679ad91618d1d63420608
yohni <yohni.123@gmail.com>
2020-05-25 14:53:38 +0700

    Create not found page with css modul

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-full" :class="$style.centered">
+    <h1 :class="$style.headingLarge">Not Found</h1>
+    <p :class="$style.textLarge">Ooopss, we couldn't find what you are looking for. Why don't you 
+      <router-link :to="{name: 'Home'}">go home instead?</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style module>
+  .headingLarge {
+    font-size: 100px;
+  }
+  .textLarge {
+    font-size: 50px;
+  }
+  .centered {
+    text-align: center;
+  }
+</style>
