39fdb2ee2ebd53027d91511c6cee00f9338f931e
yohni <yohni.123@gmail.com>
2020-05-25 14:34:34 +0700

    Adding new dir for pages components

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-full">
+    <h2>Welcome to The Forum</h2>
+    <ThreadList :threads='threads' />
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+import ThreadList from '../components/ThreadList'
+
+export default {
+  components: {
+    ThreadList
+  },
+  data () {
+    return {
+      threads: Object.values(sourceData.threads),
+      users: sourceData.users,
+      posts: sourceData.posts
+    }
+  }
+}
+</script>
