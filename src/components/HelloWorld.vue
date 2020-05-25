15a35d5cb1e114be4a318a27f9631516d9c1f4fe
yohni <yohni.123@gmail.com>
2020-05-25 13:16:06 +0700

    Implement threadlist component split

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,22 @@
 <template>
-  <div>
-    <div v-for='thread in threads' :key='thread.key' class='col-large push-top'>
-      <h1>{{thread.title}}</h1>
-
-      <div class='post-list'>
-        <div v-for='postId in thread.posts' :key='postId' class='post'>
-          <div class='user-info'>
-            <a href='#' class='user-name'>{{users[posts[postId].userId].name}}</a>
-
-            <a href='#'>
-              <img class='avatar-large' :src='users[posts[postId].userId].avatar' alt />
-            </a>
-
-            <p class='desktop-only text-small'>10 posts</p>
-          </div>
-
-          <div class='post-content'>
-            <div>{{posts[postId].text}}</div>
-          </div>
-
-          <div class='post-date text-faded'>{{posts[postId].publishedAt}}</div>
-        </div>
-      </div>
-    </div>
+  <div class="col-full">
+    <h2>Welcome to The Forum</h2>
+    <ThreadList :threads='threads' />
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
+import ThreadList from './ThreadList'
 
 export default {
   name: 'HelloWorld',
+  components: {
+    ThreadList
+  },
   data () {
     return {
-      threads: sourceData.threads,
+      threads: Object.values(sourceData.threads),
       users: sourceData.users,
       posts: sourceData.posts
     }
