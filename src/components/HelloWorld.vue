9a11572617392d0776c8c4b3ab43270cf813f650
yohni <yohni.123@gmail.com>
2020-05-25 11:51:28 +0700

    Implement style for threads page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,41 @@
 <template>
   <div>
-    <h1>Welcome to The Forum</h1>
+    <div v-for='thread in threads' :key='thread.key' class='col-large push-top'>
+      <h1>{{thread.title}}</h1>
+
+      <div class='post-list'>
+        <div v-for='postId in thread.posts' :key='postId' class='post'>
+          <div class='user-info'>
+            <a href='#' class='user-name'>{{users[posts[postId].userId].name}}</a>
+
+            <a href='#'>
+              <img class='avatar-large' :src='users[posts[postId].userId].avatar' alt />
+            </a>
+
+            <p class='desktop-only text-small'>10 posts</p>
+          </div>
+
+          <div class='post-content'>
+            <div>{{posts[postId].text}}</div>
+          </div>
+
+          <div class='post-date text-faded'>{{posts[postId].publishedAt}}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
 
-console.log(sourceData)
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      threads: sourceData.threads,
+      users: sourceData.users,
+      posts: sourceData.posts
     }
   }
 }
