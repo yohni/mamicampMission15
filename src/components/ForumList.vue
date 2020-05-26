c80b7af80c5600b10378d9b0048ad1bc1e8430e0
yohni <yohni.123@gmail.com>
2020-05-26 15:56:09 +0700

    Implement forum list item

--- src/components/ForumList.vue
+++ src/components/ForumList.vue
@@ -1,13 +1,27 @@
 <template>
-  
+  <div>
+    <ForumListItem 
+      v-for="forum in forums" 
+      :key="forum['.key']"
+      :forum='forum' />
+
+  </div>
 </template>
 
 <script>
+import ForumListItem from './ForumLIstItem'
 export default {
-
+  components: {
+    ForumListItem
+  },
+  props: {
+    forums: {
+      required: true,
+      type: Array
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
