322d946a0a9f0c599a8a1b2223df743bbcc4449e
yohni <yohni.123@gmail.com>
2020-05-26 15:59:43 +0700

    Create category page

---
+++ src/pages/PageCategory.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-full">
+    <h1>{{ category.name }}</h1>
+    <CategoryListItem :category='category' />
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+import CategoryListItem from '@/components/CategoryListItem'
+export default {
+  components: {
+    CategoryListItem
+  },
+  props: {
+    id: {
+      required: true,
+      type: String
+    }
+  },
+  computed: {
+    category () {
+      return sourceData.categories[this.id]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
