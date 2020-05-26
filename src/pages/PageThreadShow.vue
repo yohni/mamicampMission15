36dc52cff5a2486d308b562fab59b39b67927658
yohni <yohni.123@gmail.com>
2020-05-26 07:38:07 +0700

    Create new method for adding post

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -3,6 +3,21 @@
     <div class='col-large push-top'>
       <h1>{{thread.title}}</h1>
       <PostList :posts="posts" />
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea 
+            name="" 
+            id="" 
+            cols="30" 
+            rows="10"
+            class="form-input"
+            v-model="newPostText"
+            ></textarea>
+        </div>
+        <div class="form-action">
+          <button class="btn-blue">Submit post</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -20,7 +35,8 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id]
+      thread: sourceData.threads[this.id],
+      newPostText: ''
     }
   },
   components: {
@@ -31,6 +47,23 @@ export default {
       const postIds = Object.values(this.thread.posts)
       return Object.values(sourceData.posts)
         .filter(post => postIds.includes(post['.key']))
+    }
+  },
+  methods: {
+    addPost () {
+      const postId = 'greatPost' + Math.random()
+      const post = {
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: '7uVPJS9GHoftN58Z2MXCYDqmNAh2',
+        '.key': postId
+      }
+      this.$set(sourceData.posts, postId, post)
+      this.$set(this.thread.posts, postId, postId)
+      this.$set(sourceData.users[post.userId].posts, postId, postId)
+
+      this.newPostText = ''
     }
   }
 }
