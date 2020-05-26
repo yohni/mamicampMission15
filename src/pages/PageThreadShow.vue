b79d2072df480f9c07cc9cc0ae78f6792ac01853
yohni <yohni.123@gmail.com>
2020-05-26 07:11:59 +0700

    Break pagethreadshow to splited components

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,32 +2,14 @@
   <div>
     <div class='col-large push-top'>
       <h1>{{thread.title}}</h1>
-
-      <div class='post-list'>
-        <div v-for='postId in thread.posts' :key='postId' class='post'>
-          <div class='user-info'>
-            <a href='#' class='user-name'>{{users[posts[postId].userId].name}}</a>
-
-            <a href='#'>
-              <img class='avatar-large' :src='users[posts[postId].userId].avatar' alt />
-            </a>
-
-            <p class='desktop-only text-small'>10 posts</p>
-          </div>
-
-          <div class='post-content'>
-            <div>{{posts[postId].text}}</div>
-          </div>
-
-          <div class='post-date text-faded'>{{posts[postId].publishedAt}}</div>
-        </div>
-      </div>
+      <PostList :posts="posts" />
     </div>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import PostList from '@/components/PostList'
 console.log(sourceData)
 export default {
   props: {
@@ -38,9 +20,17 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id],
-      posts: sourceData.posts,
-      users: sourceData.users
+      thread: sourceData.threads[this.id]
+    }
+  },
+  components: {
+    PostList
+  },
+  computed: {
+    posts () {
+      const postIds = Object.values(this.thread.posts)
+      return Object.values(sourceData.posts)
+        .filter(post => postIds.includes(post['.key']))
     }
   }
 }
