8b595debda01f384b4d21d275b56a97c7f02df1d
yohni <yohni.123@gmail.com>
2020-05-26 08:33:37 +0700

    Split form of new post to a component

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea name id cols="30" rows="10" class="form-input" v-model="text"></textarea>
+      </div>
+      <div class="form-action">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    threadId: {
+      required: true
+    }
+  },
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    save () {
+      const postId = 'greatPost' + Math.random()
+      const post = {
+        text: this.text,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.threadId,
+        userId: '7uVPJS9GHoftN58Z2MXCYDqmNAh2',
+        '.key': postId
+      }
+
+      this.$emit('save', {post})
+
+      this.text = ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
