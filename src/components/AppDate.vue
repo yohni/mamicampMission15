c46f6719ff88142b0971bec6350ae7fae5cd8613
yohni <yohni.123@gmail.com>
2020-05-26 14:50:40 +0700

    Create date filter to base component globally

---
+++ src/components/AppDate.vue
@@ -0,0 +1,27 @@
+<template>
+  <span :title="timestamp | humanFriendlyDate" >{{timestamp | diffforHumans}}</span>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  props: {
+    timestamp: {
+      required: true,
+      type: Number
+    }
+  },
+  filters: {
+    humanFriendlyDate (date) {
+      return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
+    },
+    diffforHumans (date) {
+      return moment.unix(date).fromNow()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -15,14 +15,13 @@
         <div>{{post.text}}</div>
       </div>
 
-      <div class="post-date text-faded" :title="post.publishedAt | humanFriendlyDate" >{{post.publishedAt | diffforHumans}}</div>
+      <div class="post-date text-faded" ><AppDate :timestamp='post.publishedAt' /></div>
     </div>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
-import moment from 'moment'
 export default {
   props: {
     post: {
@@ -36,14 +35,6 @@ export default {
     },
     userPostCount () {
       return Object.keys(this.user.posts).length
-    }
-  },
-  filters: {
-    humanFriendlyDate (date) {
-      return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
-    },
-    diffforHumans (date) {
-      return moment.unix(date).fromNow()
     }
   }
 }
