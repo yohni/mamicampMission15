e21af5724395a387fe53b580db42ab49f0b9c565
yohni <yohni.123@gmail.com>
2020-05-26 21:22:01 +0700

    Remove local data

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -6,16 +6,14 @@
 </template>
 
 <script>
-import sourceData from '@/data'
 import CategoryList from '../components/CategoryList'
-console.log(sourceData)
 export default {
   components: {
     CategoryList
   },
-  data () {
-    return {
-      categories: Object.values(sourceData.categories)
+  computed: {
+    categories () {
+      return Object.values(this.$store.state.categories)
     }
   }
 }
