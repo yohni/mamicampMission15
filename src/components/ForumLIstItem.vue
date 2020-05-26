a288287696bc00a512dc678f4df11fcd197d69d2
yohni <yohni.123@gmail.com>
2020-05-26 15:59:08 +0700

    Update forumlist item component

---
+++ src/components/ForumLIstItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="forum-listing">
+    <div class="forum-details">
+      <router-link 
+        :to="{name: 'Forum', params: {id: forum['.key']}}"
+        class="text-xlarge"
+      >
+        {{forum.name}}
+      </router-link>
+      <p>{{forum.description}}</p>
+    </div>
+
+    <div class="threads-count">
+      <p>
+        <span class="count">{{threadCount}}</span> 
+        {{threadCount === 1 ? 'thread' : 'threads'}}
+      </p>
+    </div>
+
+    <div class="last-thread">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    forum: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+    threadCount () {
+      return this.forum.threads ? Object.values(this.forum.threads).length : 0
+    }
+  }
+}
+</script>
+
+<style>
+</style>
