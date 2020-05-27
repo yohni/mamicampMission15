de637f038359aac9226dff04e1cead7dcd0a0d97
yohni <yohni.123@gmail.com>
2020-05-27 18:14:40 +0700

    Create thread editor component

---
+++ src/components/ThreadEditor.vue
@@ -0,0 +1,70 @@
+<template>
+  <form @submit.prevent="save">
+      <div class="form-group">
+        <label for="thread_title">Title:</label>
+        <input 
+          v-model="form.title"
+          type="text" 
+          id="thread_title" 
+          class="form-input" 
+          name="title" />
+      </div>
+
+      <div class="form-group">
+        <label for="thread_content">Content:</label>
+        <textarea 
+          v-model="form.text"
+          id="thread_content" 
+          class="form-input" 
+          name="content" 
+          rows="8" 
+          cols="140">
+        </textarea>
+      </div>
+
+      <div class="btn-group">
+        <button @click="cancel" class="btn btn-ghost">Cancel</button>
+        <button class="btn btn-blue" type="submit" name="Publish">{{isUpdate ? 'Update' : 'Publish'}}</button>
+      </div>
+    </form>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      form: {
+        text: this.text,
+        title: this.title
+      }
+    }
+  },
+  computed: {
+    isUpdate () {
+      return !!this.title
+    }
+  },
+  methods: {
+    save () {
+      this.$emit('save', {text: this.form.text, title: this.form.title})
+    },
+    cancel () {
+      this.$emit('cancel')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
