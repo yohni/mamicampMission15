3a6e3cf34de1d2b41d73d8273cd35aeb01a0e5ae
yohni <yohni.123@gmail.com>
2020-05-27 18:12:24 +0700

    Remove useless tags

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <div class='post-list'>
-        <PostListItem v-for="post in posts" :key="post['.key']" :post="post" />
-      </div>
+  <div class='post-list'>
+    <PostListItem v-for="post in posts" :key="post['.key']" :post="post" />
   </div>
 </template>
 
