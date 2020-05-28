2032a745ebcc6011f409c904738bc178f50a859d
yohni <yohni.123@gmail.com>
2020-05-28 07:47:02 +0700

    Add firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import firebase from 'firebase'
+import firebaseConfig from './firebase/init'
 import App from './App'
 import router from './router'
 import store from '@/store'
@@ -9,6 +11,9 @@ import AppDate from '@/components/AppDate'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
