a1beab0b562a58137ea6b1d7ce91ac524dc7d900
yohni <yohni.123@gmail.com>
2020-05-28 07:42:40 +0700

    Make user able to edit the post

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -1,40 +1,64 @@
 <template>
-  <div>
-    <div class="post">
-      <div class="user-info">
-        <a href="#" class="user-name">{{user.name}}</a>
+  <div class="post">
+    <div class="user-info">
+      <a href="#" class="user-name">{{user.name}}</a>
 
-        <a href="#">
-          <img class="avatar-large" :src="user.avatar" alt />
-        </a>
+      <a href="#">
+        <img class="avatar-large" :src="user.avatar" alt />
+      </a>
+      <p class="desktop-only text-small">{{userThreadCount}} threads</p>
+      <p class="desktop-only text-small">{{userPostCount}} posts</p>
+    </div>
 
-        <p class="desktop-only text-small">{{userPostCount}} posts</p>
+    <div class="post-content">
+      <template v-if="!editing">
+        <div>
+          {{post.text}}
+        </div>
+        <a @click.prevent="editing = true" href="#" style="margin-left: auto;" class="link-unstyled" title="Make a change"><i class="fa fa-pencil"></i></a>
+      </template>
+      <div v-else>
+        <PostEditor
+          :post="post"
+          @save="editing = false"
+          @cancel="editing = false"
+        />
       </div>
+    </div>
 
-      <div class="post-content">
-        <div>{{post.text}}</div>
-      </div>
-
-      <div class="post-date text-faded" ><AppDate :timestamp='post.publishedAt' /></div>
+    <div class="post-date text-faded" >
+      <div v-if="post.edited" class="post-edition">edited</div>
+      <AppDate :timestamp='post.publishedAt' />
     </div>
   </div>
 </template>
 
 <script>
-import {countObjectProperties} from '@/utils'
+import PostEditor from './PostEditor'
 export default {
+  components: {
+    PostEditor
+  },
   props: {
     post: {
       required: true,
       type: Object
     }
   },
+  data () {
+    return {
+      editing: false
+    }
+  },
   computed: {
     user () {
       return this.$store.state.users[this.post.userId]
     },
+    userThreadCount () {
+      return this.$store.getters.userThreadsCount(this.post.userId)
+    },
     userPostCount () {
-      return countObjectProperties(this.user.posts)
+      return this.$store.getters.userPostsCount(this.post.userId)
     }
   }
 }
