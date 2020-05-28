2b796cd2cedc60bc5656d507cde0482c8ce9a9ff
yohni <yohni.123@gmail.com>
2020-05-28 07:43:48 +0700

    Add condition for update and create

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -1,37 +1,68 @@
 <template>
-  <div>
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <textarea name id cols="30" rows="10" class="form-input" v-model="text"></textarea>
-      </div>
-      <div class="form-action">
-        <button class="btn-blue">Submit post</button>
-      </div>
-    </form>
-  </div>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <textarea 
+        name 
+        id 
+        cols="30" 
+        rows="10" 
+        class="form-input" 
+        v-model="text"></textarea>
+    </div>
+    <div class="form-actions">
+      <button v-if="isUpdate" @click="cancel" class="btn btn-ghost">Cancel</button>
+      <button class="btn-blue">{{isUpdate ? 'Update' : 'Submit post'}}</button>
+    </div>
+  </form>
 </template>
 
 <script>
 export default {
   props: {
     threadId: {
-      required: true
+      required: false
+    },
+    post: {
+      type: Object
     }
   },
   data () {
     return {
-      text: ''
+      text: this.post ? this.post.text : ''
+    }
+  },
+  computed: {
+    isUpdate () {
+      return !!this.post
     }
   },
   methods: {
+    cancel () {
+      this.$emit('cancel')
+    },
     save () {
+      this.persist()
+        .then(post => {
+          this.$emit('save', {post})
+        })
+    },
+    create () {
       const post = {
         text: this.text,
         threadId: this.threadId
       }
-
-      this.$store.dispatch('createPost', post)
       this.text = ''
+      return this.$store.dispatch('createPost', post)
+    },
+    update () {
+      const payload = {
+        id: this.post['.key'],
+        text: this.text
+      }
+      return this.$store.dispatch('updatePost', payload)
+    },
+    persist () {
+      return this.isUpdate ? this.update() : this.create()
     }
   }
 }
