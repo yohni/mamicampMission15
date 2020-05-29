8b4edcf6b737027a7fe65b6c0c81efbd16a56648
yohni <yohni.123@gmail.com>
2020-05-29 23:47:45 +0700

    Create page registration

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/PageHome'
+import Register from '@/pages/PageRegister'
 import ThreadShow from '@/pages/PageThreadShow'
 import NotFound from '@/pages/PageNotFound'
 import Category from '@/pages/PageCategory'
@@ -18,6 +19,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     },
     {
       path: '/thread/create/:forumId',
