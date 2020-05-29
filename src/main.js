a30223fc965c5cc1c0a465e51577eb504dc3f9f6
yohni <yohni.123@gmail.com>
2020-05-29 19:21:58 +0700

    Fetch user data on before create rooted mode

--- src/main.js
+++ src/main.js
@@ -21,5 +21,8 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  beforeCreate () {
+    store.dispatch('fetchUser', {id: store.state.authId})
+  }
 })
