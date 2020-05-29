3ec9b4b1313204c59c3d6b43bd77a12ee8bf453d
yohni <yohni.123@gmail.com>
2020-05-29 19:23:26 +0700

    Fetch thread data

--- src/pages/PageThreadEdit.vue
+++ src/pages/PageThreadEdit.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-full push-top">
+  <div v-if="thread && text" class="col-full push-top">
     <h1>
       Editing 
       <i>{{thread.title}}</i>
@@ -15,6 +15,7 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex'
 import ThreadEditor from '@/components/ThreadEditor'
 export default {
   components: {
@@ -31,12 +32,14 @@ export default {
       return this.$store.state.threads[this.id]
     },
     text () {
-      return this.$store.state.posts[this.thread.firstPostId].text
+      const post = this.$store.state.posts[this.thread.firstPostId]
+      return post ? post.text : null
     }
   },
   methods: {
+    ...mapActions(['updateThread', 'fetchThread', 'fetchPost']),
     save ({text, title}) {
-      this.$store.dispatch('updateThread', {
+      this.updateThread({
         id: this.id,
         title,
         text }).then(thread => {
@@ -46,6 +49,10 @@ export default {
     cancel () {
       this.$router.push({name: 'ThreadShow', params: {id: this.id}})
     }
+  },
+  created () {
+    this.fetchThread({id: this.id})
+      .then(thread => this.fetchPost({id: thread.firstPostId}))
   }
 }
 </script>
