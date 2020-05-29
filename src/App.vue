048e97844af9c14f8dfaa2871c021dd2f7132c64
yohni <yohni.123@gmail.com>
2020-05-29 23:39:07 +0700

    Configure conditional rendering for spinner

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,49 @@
   <div id="app">
     <TheNavBar />
     <div class="container push-top">
-      <router-view/>
+      <router-view v-show="showPage" @ready="pageReady" />
+      <AppSpinner v-show="!showPage" />
     </div>
   </div>
 </template>
 
 <script>
 import TheNavBar from './components/TheNavBar'
+import AppSpinner from './components/AppSpinner'
+import NProgress from 'nprogress'
 export default {
   name: 'app',
   components: {
-    TheNavBar
+    TheNavBar,
+    AppSpinner
+  },
+  data () {
+    return {
+      showPage: false
+    }
+  },
+  methods: {
+    pageReady () {
+      this.showPage = true
+      NProgress.done()
+    }
+  },
+  created () {
+    NProgress.configure({
+      speed: 200,
+      showSpinner: false
+    })
+    NProgress.start()
+    this.$router.beforeEach((to, from, next) => {
+      this.showPage = false
+      NProgress.start()
+      next()
+    })
   }
 }
 </script>
 
 <style>
 @import './assets/css/style.css';
+@import '~nprogress/nprogress.css';
 </style>
