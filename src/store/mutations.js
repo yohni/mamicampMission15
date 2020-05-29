5ae8c664278cfbe0a0110435410823d894fe3a39
yohni <yohni.123@gmail.com>
2020-05-29 19:20:33 +0700

    Set local data with high order function

---
+++ src/store/mutations.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+
+const makeAppendChildtoParentMutation = ({parent, child}) =>
+  (state, {childId, parentId}) => {
+    const resource = state[parent][parentId]
+    if (!resource[child]) {
+      Vue.set(resource, child, {})
+    }
+    Vue.set(resource[child], childId, childId)
+  }
+
+export default {
+  setPost (state, {post, postId}) {
+    Vue.set(state.posts, postId, post)
+  },
+  setUser (state, {user, userId}) {
+    Vue.set(state.users, userId, user)
+  },
+  setThread (state, {thread, threadId}) {
+    Vue.set(state.threads, threadId, thread)
+  },
+  setItem (state, {item, id, resource}) {
+    item['.key'] = id
+    Vue.set(state[resource], id, item)
+  },
+  appendPostToThread: makeAppendChildtoParentMutation({parent: 'threads', child: 'posts'}),
+  appendContributorToThread: makeAppendChildtoParentMutation({parent: 'threads', child: 'contributors'}),
+  appendPostToUser: makeAppendChildtoParentMutation({parent: 'users', child: 'posts'}),
+  appendThreadToForum: makeAppendChildtoParentMutation({parent: 'forums', child: 'threads'}),
+  appendThreadToUser: makeAppendChildtoParentMutation({parent: 'users', child: 'threads'})
+}
