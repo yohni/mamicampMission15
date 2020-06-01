fd7583ff5869282cb19e42ed76827e527f453fe6
yohni <yohni.123@gmail.com>
2020-06-01 12:19:58 +0700

    Implement navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,11 @@ import Forum from '@/pages/PageForum'
 import Profile from '@/pages/PageProfile'
 import ThreadCreate from '@/pages/PageThreadCreate'
 import ThreadEdit from '@/pages/PageThreadEdit'
+import store from '@/store'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -24,18 +25,21 @@ export default new Router({
     {
       path: '/register',
       name: 'Register',
-      component: Register
+      component: Register,
+      meta: {requiresGuest: true}
     },
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: Login,
+      meta: {requiresGuest: true}
     },
     {
       path: '/thread/create/:forumId',
       name: 'ThreadCreate',
       component: ThreadCreate,
-      props: true
+      props: true,
+      meta: {requiresAuth: true}
     },
     {
       path: '/thread/:id',
@@ -47,7 +51,8 @@ export default new Router({
       path: '/thread/:id/edit',
       name: 'ThreadEdit',
       component: ThreadEdit,
-      props: true
+      props: true,
+      meta: {requiresAuth: true}
     },
     {
       path: '/category/:id',
@@ -65,13 +70,24 @@ export default new Router({
       path: '/me',
       name: 'Profile',
       component: Profile,
+      meta: {requiresAuth: true},
       props: {editProfile: false}
     },
     {
       path: '/me/edit',
       name: 'ProfileEdit',
+      meta: {requiresAuth: true},
       component: Profile,
       props: {editProfile: true}
+    },
+    {
+      path: '/logout',
+      name: 'Logout',
+      meta: {requiresAuth: true},
+      beforeEnter (to, from, next) {
+        store.dispatch('signOut')
+          .then(() => next({name: 'Home'}))
+      }
     },
     {
       path: '*',
@@ -80,3 +96,28 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  console.log('🚦', `from ${from.name} to ${to.name}`)
+  console.log(to.matched)
+  store.dispatch('initAuthentication')
+    .then(user => {
+      if (to.matched.some(route => route.meta.requiresAuth)) {
+        if (user) {
+          next()
+        } else {
+          next({name: 'Login', query: {redirectTo: to.path}})
+        }
+      } else if (to.matched.some(route => route.meta.requiresGuest)) {
+        if (!user) {
+          next()
+        } else {
+          next({name: 'Home'})
+        }
+      } else {
+        next()
+      }
+    })
+})
+
+export default router
