26c110ee035a7fee2b17af3bd39e105732b86913
yohni <yohni.123@gmail.com>
2020-06-01 21:14:48 +0700

    Move all actions, getters, mutations, state to module

--- src/store/index.js
+++ src/store/index.js
@@ -3,23 +3,30 @@ import Vuex from 'vuex'
 import actions from './actions'
 import getters from './getters'
 import mutations from './mutations'
+import categories from './modules/categories'
+import forums from './modules/forums'
+import posts from './modules/posts'
+import threads from './modules/threads'
+import users from './modules/users'
+import auth from './modules/auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    categories: {},
-    forums: {},
-    threads: {},
-    posts: {},
-    users: {},
-    authId: null,
-    unsubscribeAuthObserver: null
-  },
+  state: {},
 
   getters,
 
   actions,
 
-  mutations
+  mutations,
+
+  modules: {
+    categories,
+    forums,
+    posts,
+    threads,
+    users,
+    auth
+  }
 })
